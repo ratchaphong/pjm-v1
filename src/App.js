23f8b9df507608c9e0e9664e4c63de1a361546a8
ratchaphong <ratchaphongc1@gamil.com>
2022-07-07 09:45:33 +0700

    remove warning.

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Title from "./title";
-import Home from "./home";
-import About from "./about";
-import Service from "./service";
+// import Home from "./home";
+// import About from "./about";
+// import Service from "./service";
 import Portfolio from "./portfolio";
 import NotFound from "./notfound";
 import Gallery from "./gallery";
