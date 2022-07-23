8a17bb902bf9e4bca80cee02a0c59ad4344e149b
ratchaphong <ratchaphongc1@gamil.com>
2022-07-23 16:38:49 +0700

    add BASE_URL

--- src/requestMethods.js
+++ src/requestMethods.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const BASE_URL = process.env.REACT_APP_API_URL;
+const BASE_URL = process.env.REACT_APP_API_URL_SIT;
 console.log("BASE_URL => ", BASE_URL);
 const user = JSON.parse(localStorage.getItem("persist:root"))?.auth;
 console.log("user => ", user);
